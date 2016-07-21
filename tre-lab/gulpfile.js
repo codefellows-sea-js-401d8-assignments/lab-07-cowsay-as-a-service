3c45ccf7a4aabd5e0f3e8c40733c0baaac57caf9
ercain03 <ercain3@gmail.com>
2016-07-21 12:35:10 -0700

    getting things started

---
+++ tre-lab/gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+
+var dirs = ['./server.js', './lib/*.js', './test/*.js'];  
+var testFiles = ['./test/*.js'];
+
+gulp.task('lint', () => {
+  return gulp.src(dirs)
+  .pipe(eslint({
+    rules: {
+      'no-console': 0,
+      'indent': [
+        2,
+        2
+      ],
+      'quotes': [
+        2,
+        'single'
+      ],
+      'linebreak-style': [
+        2,
+        'unix'
+      ],
+      'semi': [
+        2,
+        'always'
+      ]
+    },
+    env: {
+      'es6': true,
+      'node': true,
+      'browser': true
+    },
+    global: {
+      'describe': false,
+      'it': false,
+      'beforeEach': false,
+      'afterEach': false,
+      'before': false,
+      'after': false
+    },
+    ecmaFeatures: {
+      'modules': true,
+      'experimentalObjectRestSpread': true,
+      'impliedStrict': true
+    },
+    extends: 'eslint:recommended'
+  }))
+  .pipe(eslint.format());
+});
+
+gulp.task('test', () => {
+  return gulp.src(testFiles)
+  .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('watch', () => {
+  gulp.watch([dirs, testFiles], ['lint', 'test']);
+});
+
+gulp.task('default', ['watch', 'lint', 'test']);
