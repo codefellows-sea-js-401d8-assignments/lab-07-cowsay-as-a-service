e829a0473038b35b1c5e7924b461e27957e93a51
Gio d'Amelio <giodamelio@gmail.com>
2016-07-20 22:31:46 -0700

    Add post route and tests

--- lab-giodamelio/lib/server.js
+++ lab-giodamelio/lib/server.js
@@ -4,21 +4,43 @@ const url = require('url');
 const sendCowsay = require('./sendCowsay').sendCowsay;
 
 const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('API Endpoints:\n/api/cowsay');
-    return;
-  }
-
-  if (req.url.startsWith('/api/cowsay')) {
+  } else if (parsedUrl.pathname === '/api/cowsay') {
     if (req.method === 'GET') {
-      const parsedUrl = url.parse(req.url, true);
-      if (parsedUrl.query.text) {
-        sendCowsay(res, parsedUrl.query.text);
-      } else {
+      if (!parsedUrl.query.text) {
         sendCowsay(res, 'You must inclue a text query param');
+        return;
       }
+
+      sendCowsay(res, parsedUrl.query.text);
+    } else if (req.method === 'POST') {
+      let body = '';
+
+      req.on('data', (data) => {
+        body += data.toString();
+      });
+
+      req.on('end', () => {
+        try {
+          const options = JSON.parse(body);
+          if (!options.text) {
+            sendCowsay(res, 'You must include a json body with a text property');
+            return;
+          }
+
+          sendCowsay(res, options.text);
+        } catch (e) {
+          sendCowsay(res, 'Invalid request');
+        }
+      });
+    } else {
+      sendCowsay(res, 'Only GET and POST requests allowed');
     }
+  } else {
+    sendCowsay(res, 'Invalid route');
   }
 });
 
--- lab-giodamelio/test/server_test.js
+++ lab-giodamelio/test/server_test.js
@@ -12,22 +12,75 @@ describe('Server', () => {
       .end(done);
   });
 
-  describe('GET /api/cowsay', () => {
-    it('Simple get', (done) => {
+  describe('/api/cowsay', () => {
+    describe('GET', () => {
+      it('Simple get', (done) => {
+        supertest(server)
+          .get('/api/cowsay?text=Hello World')
+          .expect(200)
+          .expect('Content-Type', 'text/plain')
+          .expect(' _____________\n< Hello World >\n -------------\n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||') // eslint-disable-line max-len
+          .end(done);
+      });
+
+      it('No text query param', (done) => {
+        supertest(server)
+          .get('/api/cowsay')
+          .expect(200)
+          .expect('Content-Type', 'text/plain')
+          .expect(' ____________________________________\n< You must inclue a text query param >\n ------------------------------------\n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||') // eslint-disable-line max-len
+          .end(done);
+      });
+    });
+
+    describe('POST', () => {
+      it('Simple post', (done) => {
+        supertest(server)
+          .post('/api/cowsay')
+          .send({
+            text: 'Hello From Post!',
+          })
+          .expect(200)
+          .expect('Content-Type', 'text/plain')
+          .expect(' __________________\n< Hello From Post! >\n ------------------\n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||') // eslint-disable-line max-len
+          .end(done);
+      });
+
+      it('No text property', (done) => {
+        supertest(server)
+          .post('/api/cowsay')
+          .send({})
+          .expect(200)
+          .expect('Content-Type', 'text/plain')
+          .expect(' ___________________________________________________\n< You must include a json body with a text property >\n ---------------------------------------------------\n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||') // eslint-disable-line max-len
+          .end(done);
+      });
+
+      it('Non json body', (done) => {
+        supertest(server)
+          .post('/api/cowsay')
+          .expect(200)
+          .expect('Content-Type', 'text/plain')
+          .expect(' _________________\n< Invalid request >\n -----------------\n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||') // eslint-disable-line max-len
+          .end(done);
+      });
+    });
+
+    it('Invalid method', (done) => {
       supertest(server)
-        .get('/api/cowsay?text=Hello World')
+        .put('/api/cowsay')
         .expect(200)
         .expect('Content-Type', 'text/plain')
-        .expect(' _____________\n< Hello World >\n -------------\n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||') // eslint-disable-line max-len
+        .expect(' ____________________________________\n< Only GET and POST requests allowed >\n ------------------------------------\n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||') // eslint-disable-line max-len
         .end(done);
     });
 
-    it('No text query param', (done) => {
+    it('Invalid route', (done) => {
       supertest(server)
-        .get('/api/cowsay')
+        .put('/api/i_dont_exist')
         .expect(200)
         .expect('Content-Type', 'text/plain')
-        .expect(' ____________________________________\n< You must inclue a text query param >\n ------------------------------------\n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||') // eslint-disable-line max-len
+        .expect(' _______________\n< Invalid route >\n ---------------\n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||') // eslint-disable-line max-len
         .end(done);
     });
   });
