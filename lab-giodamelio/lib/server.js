5a07cf08dad547e3ca356c723741d7fa556a4059
Gio d'Amelio <giodamelio@gmail.com>
2016-07-20 19:34:22 -0700

    Basic root route and test

---
+++ lab-giodamelio/lib/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('API Endpoints:\n/api/cowsay');
+  }
+});
+
+module.exports = server;
--- lab-giodamelio/test/server_test.js
+++ lab-giodamelio/test/server_test.js
@@ -1,5 +1,15 @@
-const chai = require('chai');
+const expect = require('chai').expect;
+const supertest = require('supertest');
+
+const server = require('../lib/server');
 
 describe('Server', () => {
-
+  it('Handle / route', (done) => {
+    supertest(server)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', 'text/plain')
+      .expect('API Endpoints:\n/api/cowsay')
+      .end(done);
+  });
 });
