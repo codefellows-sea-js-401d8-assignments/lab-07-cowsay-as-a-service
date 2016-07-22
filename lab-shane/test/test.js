bd51df73958ac1442f455642d1f39f59c764fc7c
Shane Henning <shanerhenning@gmail.com>
2016-07-22 16:22:59 -0700

    test implemented

--- lab-shane/test/test.js
+++ lab-shane/test/test.js
@@ -10,9 +10,8 @@ const server = require('../lib/server.js');
 describe('Cowsay as a service testing', () => {
   it('should return a status code of "200"', (done) => {
     request('localhost:3000/api')
-      .get('/cowsay?id=hello-world')
+      .get('/cowsay?text=hello-world')
       .end(function(err, res) {
-
         expect(err).to.eql(null, 'Error is null');
         expect(res).to.have.status(200, 'this is the status code');
         done();
