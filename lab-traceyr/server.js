d4b77554b3c6c836dc94ed39535d3e3558ad6185
Tracey <radcliffe.tracey@gmail.com>
2016-07-24 15:10:26 -0700

    added promise and did basic setup of server.js file

--- lab-traceyr/server.js
+++ lab-traceyr/server.js
@@ -3,10 +3,16 @@ const fs = require('fs');
 const url = require('url');
 
 http.createServer((req, res) =>{
-  jsonPromise(req)
-  .then((json) => {
-    res.writeHead(200, {
-      'Content-Type': 'text/plain'
-    })
-  })
+  if(req.method === 'POST'){
+
+  }
+  if(req.method === 'GET'){
+
+  }
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+
+}).listen(3000, () => {
+  console.log('Server On');
 });
